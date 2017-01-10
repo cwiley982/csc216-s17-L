0982da2342daf0390967b36f923c6670a940034c
cjwiley2 <cjwiley2@eb2-1221-mmd14.csc.ncsu.edu>
2017-01-10 11:46:27 -0500

    skeleton Student class

--- PackScheduler/src/edu/ncsu/csc216/pack_scheduler/user/Student.java
+++ PackScheduler/src/edu/ncsu/csc216/pack_scheduler/user/Student.java
@@ -2,7 +2,125 @@ package edu.ncsu.csc216.pack_scheduler.user;
 
 public class Student {
 
-	public Student(String firstName, String lastName, String id, String email, String hashPW, int maxCredits) {
+	private String firstName;
+	private String lastName;
+	private String id;
+	private String email;
+	private String password;
+	private int maxCredits;
+	public static final int MAX_CREDITS = 18;
+	
+	public Student(String firstName, String lastName, String id, String email, String password, int maxCredits) {
+		setFirstName(firstName);
+		setLastName(lastName);
+		setId(id);
+		setEmail(email);
+		setPassword(password);
+		setMaxCredits(MAX_CREDITS);
+	}
+	public Student(String firstName, String lastName, String id, String email, String password) {
+		this(firstName,lastName,id,email,password,0);
+	}
+	
+
+	public String getFirstName() {
+		return firstName;
+	}
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+	public String getLastName() {
+		return lastName;
+	}
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+	public String getId() {
+		return id;
+	}
+	private void setId(String id) {
+		this.id = id;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public void setMaxCredits(int maxCredits) {
+		this.maxCredits = maxCredits;
+	}
+	public static int getMaxCredits() {
+		return MAX_CREDITS;
+	}
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((email == null) ? 0 : email.hashCode());
+		result = prime * result + ((firstName == null) ? 0 : firstName.hashCode());
+		result = prime * result + ((password == null) ? 0 : password.hashCode());
+		result = prime * result + ((id == null) ? 0 : id.hashCode());
+		result = prime * result + ((lastName == null) ? 0 : lastName.hashCode());
+		result = prime * result + maxCredits;
+		return result;
+	}
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Student other = (Student) obj;
+		if (email == null) {
+			if (other.email != null)
+				return false;
+		} else if (!email.equals(other.email))
+			return false;
+		if (firstName == null) {
+			if (other.firstName != null)
+				return false;
+		} else if (!firstName.equals(other.firstName))
+			return false;
+		if (password == null) {
+			if (other.password != null)
+				return false;
+		} else if (!password.equals(other.password))
+			return false;
+		if (id == null) {
+			if (other.id != null)
+				return false;
+		} else if (!id.equals(other.id))
+			return false;
+		if (lastName == null) {
+			if (other.lastName != null)
+				return false;
+		} else if (!lastName.equals(other.lastName))
+			return false;
+		if (maxCredits != other.maxCredits)
+			return false;
+		return true;
+	}
+	@Override
+	public String toString() {
+		return firstName + "," + lastName + "," + id + "," + email + "," + password +"," + maxCredits;
+	}
+	
+	
+	
+	
+	
+	
+	/*public Student(String firstName, String lastName, String id, String email, String password, int maxCredits) {
 		// TODO Auto-generated constructor stub
 	}
 	
@@ -19,6 +137,12 @@ public class Student {
 	public String getId() {
 		// TODO Auto-generated method stub
 		return null;
-	}
+	}*/
+	
+	
+	
+	
+	
+	
 
 }
