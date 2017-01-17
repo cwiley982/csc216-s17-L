f26b8fd1a284477c0e5fce57bf87e96c3e758996
Caitlyn <Caitlyn@Caitlyns-Laptop>
2017-01-17 12:16:28 -0500

    Updated StudentRecordIO.java

--- PackScheduler/src/edu/ncsu/csc216/pack_scheduler/io/StudentRecordIO.java
+++ PackScheduler/src/edu/ncsu/csc216/pack_scheduler/io/StudentRecordIO.java
@@ -1,8 +1,13 @@
 package edu.ncsu.csc216.pack_scheduler.io;
 
+import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.IOException;
+import java.io.PrintStream;
 import java.util.ArrayList;
+import java.util.InputMismatchException;
+import java.util.NoSuchElementException;
+import java.util.Scanner;
 
 import edu.ncsu.csc216.pack_scheduler.user.Student;
 
@@ -11,19 +16,71 @@ public class StudentRecordIO {
 	public StudentRecordIO(){
 		
 	}
-	
-	private static Student processStudent(String studentInfo){
-		
-		return null; //For now
-	}
+
 	public static ArrayList<Student> readStudentRecords(String fileName) throws FileNotFoundException {
-		// TODO Auto-generated method stub
-		return null;
+		Scanner fileReader = new Scanner(new File(fileName));
+		ArrayList<Student> students = new ArrayList<Student>();
+		while (fileReader.hasNextLine()) {
+			try {
+				Student student = processStudent(fileReader.nextLine());
+				boolean duplicate = false;
+				for (int i = 0; i < students.size(); i++) {
+					Student c = students.get(i);
+					if (student.getId().equals(c.getId())) {
+						// it's a duplicate
+						duplicate = true;
+					} 
+				}
+				if (!duplicate) {
+					students.add(student);
+				}
+			} catch (IllegalArgumentException | NullPointerException e) {
+
+			}
+		}
+
+
+		fileReader.close();
+		return students;
+
 	}
 
 	public static void writeStudentRecords(String fileName, ArrayList<Student> studentDirectory) throws IOException {
-		// TODO Auto-generated method stub
-		
+		PrintStream writer = new PrintStream(new File(fileName));
+		for (int i = 0; i < studentDirectory.size(); i++) {
+			writer.println(studentDirectory.get(i).toString());
+		}
+		writer.close();
+
+	}
+
+	private static Student processStudent(String studentInfo) {
+		Scanner info = new Scanner(studentInfo);
+		info.useDelimiter(",");
+		Student s = null;
+
+		try {
+			String firstName = info.next();
+			String lastName = info.next();
+			String id = info.next();
+			String email = info.next();
+			String password = info.next();
+			try {
+				int maxCredits = info.nextInt();
+				info.close();
+				s = new Student(firstName, lastName, id, email, password, maxCredits);
+
+			} catch (NoSuchElementException e) {
+				info.close();
+				s = new Student(firstName, lastName, id, email, password);
+
+			}
+
+		} catch (NoSuchElementException e) {
+
+		}
+
+		return s;
 	}
 
 }
